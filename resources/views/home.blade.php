69ce0312d1ce132d10465e0eca4e68365f13e3bf
Reyhan33 <ryakuzenkiwami@gmail.com>
2019-08-12 08:59:28 +0700

    Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Sanctuary')
+
+@section('content_header')
+    <h1>You Hard To Contact Spiderman!</h1>
+@stop
+
+@section('content')
+    <p>You are logged in!</p>
+@stop
